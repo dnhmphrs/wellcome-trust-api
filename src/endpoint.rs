4b7e445fa7f06689a2c87ccc1eeea3c026af5b0e
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-12-05 13:30:08 +0000

    restructure + outline Sumulative Report Code

--- src/endpoint.rs
+++ src/endpoint.rs
@@ -1,7 +1,8 @@
-use std::{time::Instant};
+use std::{str, time::Instant};
 
 use actix_web::{get, web::Data, HttpResponse};
 use awc::{Client};
+use serde_json::Value;
 
 // CONSTS
 const MAP_URL: &str =
@@ -33,6 +34,34 @@ pub async fn fetch_data(client: Data<Client>) -> HttpResponse {
         start.elapsed().as_millis()
     );
 
+    create_summary_report(&payload);
+
     HttpResponse::Ok()
         .body(payload)
+}
+
+// CREATE SUMULATIVE REPORT
+fn create_sumulative_report(data: &actix_web::web::Bytes) {
+    // log::info!("{}", str::from_utf8(data).unwrap());
+
+    // get json from data
+    let json: Value = serde_json::from_slice(data).unwrap();
+
+    for i in 0..json["items"].as_array().expect("Array Expected").len() {
+        log::info!("{}", i);
+        log::info!("{}", json["items"][i]);
+    }
+
+    // The total number of submitted, approved and rejected applications per research area
+
+    // For each of the past 12 months:
+    // - the total submitted, approved, and rejected applications in each month
+
+    // - the sum of funding we approved in each month based on the applications data.
+
+    // The average time in (days) between an application being received (submitted) and an outcome (approved or rejected)
+
+    // A list of application ids which have not been actioned in more than 60 days from their submitted date (i.e. they are still in the submitted state).
+
+    // Return bytes object for HTTP Response
 }
